f9e04e552d0c2f4f93b6b91286e4d0d4bdbd6bc8
Scott Mangiapane <scottmangiapane@gmail.com>
2016-12-23 11:09:10 -0700

    Added apple, stopped player from reversing direction

--- game.js
+++ game.js
@@ -7,33 +7,40 @@ display.style.height = (displaySize / 2) + "px";
 
 let blockSize = displaySize / 50;
 
+let apple = {x: Math.random() * 50, y: Math.random() * 50};
 let snakeP1 = [];
 let snakeP2 = [];
+
+let direction = 0;
 
 snakeP1.push({x: 5, y: 5});
 snakeP2.push({x: 44, y: 44});
 
 document.onkeydown = (event) => {
     event = event || window.event;
-    if (event.keyCode == 38) {
-        console.log("UP");
-    }
-    if (event.keyCode == 39) {
-        console.log("RIGHT");
-    }
-    if (event.keyCode == 40) {
-        console.log("DOWN");
-    }
-    if (event.keyCode == 37) {
-        console.log("LEFT");
-    }
+    if (event.keyCode == 38 && direction != 3)
+        direction = 1;
+    if (event.keyCode == 39 && direction != 4)
+        direction = 2;
+    if (event.keyCode == 40 && direction != 1)
+        direction = 3;
+    if (event.keyCode == 37 && direction != 2)
+        direction = 4;
 };
 
 requestAnimationFrame(frame);
 
+function update() {
+
+}
+
 function frame() {
     requestAnimationFrame(frame);
     context.clearRect(0, 0, displaySize, displaySize);
+    context.fillStyle = "#8BC34A";
+    context.beginPath();
+    context.rect(apple.x * blockSize, apple.y * blockSize, blockSize, blockSize);
+    context.fill();
     context.fillStyle = "#03A9F4";
     context.beginPath();
     for (let i = 0; i < snakeP1.length; i++)
