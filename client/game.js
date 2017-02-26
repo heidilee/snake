5d8004bf4671d61edbe5c497967f5e1c1a295ac1
Scott Mangiapane <scottmangiapane@gmail.com>
2017-02-26 15:33:46 -0500

    Game is now playable over a server

--- client/game.js
+++ client/game.js
@@ -1,6 +1,7 @@
 const display = document.getElementById("canvas");
 const context = display.getContext("2d");
 const gridSize = 40;
+const socket = io();
 
 let displaySize = display.width = display.height = Math.min(window.innerWidth, window.innerHeight) * 2;
 let blockSize = displaySize / gridSize;
@@ -8,112 +9,68 @@ let blockSize = displaySize / gridSize;
 display.style.width = (displaySize / 2) + "px";
 display.style.height = (displaySize / 2) + "px";
 
-let apple = {x: parseInt(Math.random() * gridSize), y: parseInt(Math.random() * gridSize)};
-let snakes = [{
-    direction: 0,
-    directionRequest: 0,
-    colorLight: "#2196F3",
-    colorDark: "#1E88E5",
-    keyCodes: [87, 68, 83, 65],
-    nodes: [{x: 5, y: 5}],
-    score: 0,
-}, {
-    direction: 0,
-    directionRequest: 0,
-    colorLight: "#f44336",
-    colorDark: "#e53935",
-    keyCodes: [38, 39, 40, 37],
-    nodes: [{x: gridSize - 6, y: gridSize - 6}],
-    score: 0,
-}];
+let apple = {x: 0, y: 0};
+let snakes = {};
 
-document.onkeydown = (event) => {
-    event = event || window.event;
-    snakes.forEach((snake) => {
-        if (event.keyCode === snake.keyCodes[0])
-            snake.directionRequest = 1;
-        if (event.keyCode === snake.keyCodes[1])
-            snake.directionRequest = 2;
-        if (event.keyCode === snake.keyCodes[2])
-            snake.directionRequest = 3;
-        if (event.keyCode === snake.keyCodes[3])
-            snake.directionRequest = 4;
-    });
+socket.on("update", function (data) {
+	apple = data.apple;
+	snakes = data.snakes;
+	requestAnimationFrame(frame);
+});
+
+let player = {
+	direction: 0,
+	directionRequest: 0,
+	colorLight: "#2196F3",
+	colorDark: "#1E88E5",
+	id: "sket" + Math.random(),
+	keyCodes: [87, 68, 83, 65],
+	nodes: [{x: 5, y: 5}],
+	score: 0,
 };
 
-window.onresize = () => {
-    displaySize = display.width = display.height = Math.min(window.innerWidth, window.innerHeight) * 2;
-    blockSize = displaySize / gridSize;
-    display.style.width = (displaySize / 2) + "px";
-    display.style.height = (displaySize / 2) + "px";
+socket.emit("joinGame", player);
+
+document.onkeydown = function (event) {
+	event = event || window.event;
+	if (event.keyCode === player.keyCodes[0])
+		player.directionRequest = 1;
+	if (event.keyCode === player.keyCodes[1])
+		player.directionRequest = 2;
+	if (event.keyCode === player.keyCodes[2])
+		player.directionRequest = 3;
+	if (event.keyCode === player.keyCodes[3])
+		player.directionRequest = 4;
+	socket.emit("keyPress", {id: player.id, direction: player.directionRequest});
 };
 
-requestAnimationFrame(frame);
+window.onresize = function () {
+	displaySize = display.width = display.height = Math.min(window.innerWidth, window.innerHeight) * 2;
+	blockSize = displaySize / gridSize;
+	display.style.width = (displaySize / 2) + "px";
+	display.style.height = (displaySize / 2) + "px";
+};
 
 function frame() {
-    setTimeout(() => {
-        requestAnimationFrame(frame);
-    }, 40);
-    context.clearRect(0, 0, displaySize, displaySize);
-    context.fillStyle = "#8BC34A";
-    context.beginPath();
-    context.rect(apple.x * blockSize, apple.y * blockSize, blockSize, blockSize);
-    context.fill();
-    snakes.forEach(updateSnake);
-    snakes.forEach(drawSnake);
-}
-
-function updateSnake(snake) {
-    if (snake.direction === 0 || snake.direction % 2 !== snake.directionRequest % 2)
-        snake.direction = snake.directionRequest;
-    let x = snake.nodes[0].x;
-    let y = snake.nodes[0].y;
-    for (let i = 1; i < snake.nodes.length; i++)
-        if (x === snake.nodes[i].x && y === snake.nodes[i].y)
-            alert("Collision");
-    if (x === apple.x && y === apple.y) {
-        apple = {
-            x: parseInt(Math.random() * gridSize),
-            y: parseInt(Math.random() * gridSize),
-        };
-        snake.score++;
-        let end = snake.nodes[snake.nodes.length - 1];
-        snake.nodes.push({x: end.x, y: end.y});
-        snake.nodes.push({x: end.x, y: end.y});
-        snake.nodes.push({x: end.x, y: end.y});
-    }
-    switch (snake.direction) {
-        case 1:
-            snake.nodes.unshift({x: x, y: y - 1});
-            snake.nodes.pop();
-            break;
-        case 2:
-            snake.nodes.unshift({x: x + 1, y: y});
-            snake.nodes.pop();
-            break;
-        case 3:
-            snake.nodes.unshift({x: x, y: y + 1});
-            snake.nodes.pop();
-            break;
-        case 4:
-            snake.nodes.unshift({x: x - 1, y: y});
-            snake.nodes.pop();
-            break;
-    }
-    snake.nodes[0].x = ((snake.nodes[0].x % gridSize) + gridSize) % gridSize;
-    snake.nodes[0].y = ((snake.nodes[0].y % gridSize) + gridSize) % gridSize;
+	context.clearRect(0, 0, displaySize, displaySize);
+	context.fillStyle = "#8BC34A";
+	context.beginPath();
+	context.rect(apple.x * blockSize, apple.y * blockSize, blockSize, blockSize);
+	context.fill();
+	for (let key in snakes)
+		drawSnake(snakes[key]);
 }
 
 function drawSnake(snake) {
-    for (let i = snake.nodes.length - 1; i >= 0; i--) {
-        if (i === 0)
-            context.fillStyle = "#FFFFFF";
-        else if (snake.nodes[i].x % 2 === snake.nodes[i].y % 2)
-            context.fillStyle = snake.colorLight;
-        else
-            context.fillStyle = snake.colorDark;
-        context.beginPath();
-        context.rect(snake.nodes[i].x * blockSize, snake.nodes[i].y * blockSize, blockSize, blockSize);
-        context.fill();
-    }
+	for (let i = snake.nodes.length - 1; i >= 0; i--) {
+		if (i === 0)
+			context.fillStyle = "#FFFFFF";
+		else if (snake.nodes[i].x % 2 === snake.nodes[i].y % 2)
+			context.fillStyle = snake.colorLight;
+		else
+			context.fillStyle = snake.colorDark;
+		context.beginPath();
+		context.rect(snake.nodes[i].x * blockSize, snake.nodes[i].y * blockSize, blockSize, blockSize);
+		context.fill();
+	}
 }
--- index.js
+++ index.js
@@ -1,24 +1,90 @@
-var express = require('express')
-var app = express();
+// SERVER
 
-app.use(express.static(__dirname + '/client'));
+let express = require("express")
+let app = express();
 
-var server = app.listen(8080, function () {
-	var port = server.address().port;
-	console.log('Server running at port %s', port);
+app.use(express.static(__dirname + "/client"));
+
+app.all("*", function(req, res) {
+	res.redirect("/");
 });
 
-var io = require('socket.io') (server);
+let server = app.listen(8080, function () {
+	let port = server.address().port;
+	console.log("Server running at port %s", port);
+});
 
-io.on('connection', function(client) {
-	console.log('User connected');
-	client.on('joinGame', function(snake) {
-		console.log(snake.id + ' joined the game');
+let clients = {};
+let io = require("socket.io") (server);
+
+// GAME
+
+const gridSize = 40;
+
+let apple = {x: parseInt(Math.random() * gridSize), y: parseInt(Math.random() * gridSize)};
+let snakes = {};
+
+io.sockets.on("connection", function(client) {
+	console.log("User connected");
+	client.on("joinGame", function(snake) {
+		console.log(snake.id + " joined the game");
+		clients[snake.id] = client;
+		console.log("Added " + snake.id + " to the client list");
+		console.log("There are " + Object.keys(clients).length + " clients total");
+		snakes[snake.id] = snake;
+	});
+	client.on("keyPress", function(keyPress) {
+		console.log(keyPress.id + " requested direction " + keyPress.direction);
+		snakes[keyPress.id].directionRequest = keyPress.direction;
 	});
 });
 
-/*var INTERVAL = 50;
+setInterval(function () {
+	for (let key in snakes) {
+		updateSnake(snakes[key]);
+	};
+	for (let key in clients) {
+		clients[key].emit('update', {apple: apple, snakes: snakes});
+	};
+}, 40);
 
-setInterval (function() {
-	g.mainLoop();
-}, INTERVAL);*/
+function updateSnake(snake) {
+	if (snake.direction === 0 || snake.direction % 2 !== snake.directionRequest % 2)
+		snake.direction = snake.directionRequest;
+	let x = snake.nodes[0].x;
+	let y = snake.nodes[0].y;
+	for (let i = 1; i < snake.nodes.length; i++)
+		if (x === snake.nodes[i].x && y === snake.nodes[i].y)
+			console.log(snake.id + " has collided");
+	if (x === apple.x && y === apple.y) {
+		apple = {
+			x: parseInt(Math.random() * gridSize),
+			y: parseInt(Math.random() * gridSize),
+		};
+		snake.score++;
+		let end = snake.nodes[snake.nodes.length - 1];
+		snake.nodes.push({x: end.x, y: end.y});
+		snake.nodes.push({x: end.x, y: end.y});
+		snake.nodes.push({x: end.x, y: end.y});
+	}
+	switch (snake.direction) {
+		case 1:
+			snake.nodes.unshift({x: x, y: y - 1});
+			snake.nodes.pop();
+			break;
+		case 2:
+			snake.nodes.unshift({x: x + 1, y: y});
+			snake.nodes.pop();
+			break;
+		case 3:
+			snake.nodes.unshift({x: x, y: y + 1});
+			snake.nodes.pop();
+			break;
+		case 4:
+			snake.nodes.unshift({x: x - 1, y: y});
+			snake.nodes.pop();
+			break;
+	}
+	snake.nodes[0].x = ((snake.nodes[0].x % gridSize) + gridSize) % gridSize;
+	snake.nodes[0].y = ((snake.nodes[0].y % gridSize) + gridSize) % gridSize;
+}
