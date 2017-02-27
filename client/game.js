a02df42db6f07c44d2a7e4953ce00eebebae4a84
Scott Mangiapane <scottmangiapane@gmail.com>
2017-02-27 10:22:09 -0500

    General linting

--- client/game.js
+++ client/game.js
@@ -2,23 +2,10 @@ const display = document.getElementById("canvas");
 const context = display.getContext("2d");
 const socket = io();
 
-const colors =
-	[	["#E53935", "#F44336"],
-		["#D81B60", "#E91E63"],
-		["#8E24AA", "#9C27B0"],
-		["#5E35B1", "#673AB7"],
-		["#3949AB", "#3F51B5"],
-		["#1E88E5", "#2196F3"],
-		["#039BE5", "#03A9F4"],
-		["#00ACC1", "#00BCD4"],
-		["#00897B", "#009688"],
-		["#43A047", "#4CAF50"],
-		["#C0CA33", "#CDDC39"],
-		["#FDD835", "#FFEB3B"],
-		["#FFB300", "#FFC107"],
-		["#FB8C00", "#FF9800"],
-		["#F4511E", "#FF5722"],
-		["#546E7A", "#607D8B"]	];
+const colors = [["#E53935", "#F44336"], ["#D81B60", "#E91E63"], ["#8E24AA", "#9C27B0"], ["#5E35B1", "#673AB7"],
+    ["#3949AB", "#3F51B5"], ["#1E88E5", "#2196F3"], ["#039BE5", "#03A9F4"], ["#00ACC1", "#00BCD4"],
+    ["#00897B", "#009688"], ["#43A047", "#4CAF50"], ["#C0CA33", "#CDDC39"], ["#FDD835", "#FFEB3B"],
+    ["#FFB300", "#FFC107"], ["#FB8C00", "#FF9800"], ["#F4511E", "#FF5722"], ["#546E7A", "#607D8B"]];
 const gridSize = 40;
 
 let displaySize = display.width = display.height = Math.min(window.innerWidth, window.innerHeight) * 2;
@@ -31,62 +18,62 @@ let apple = {};
 let snakes = {};
 
 socket.on("update", function (data) {
-	apple = data.apple;
-	snakes = data.snakes;
-	requestAnimationFrame(frame);
+    apple = data.apple;
+    snakes = data.snakes;
+    requestAnimationFrame(frame);
 });
 
 let player = {
-	color: parseInt(Math.random() * colors.length),
-	direction: 0,
-	directionRequest: 0,
-	keyCodes: [37, 38, 39, 40],
-	nodes: [{x: parseInt(Math.random() * gridSize), y: parseInt(Math.random() * gridSize)}],
-	score: 0,
+    color: parseInt(Math.random() * colors.length),
+    direction: 0,
+    directionRequest: 0,
+    keyCodes: [37, 38, 39, 40],
+    nodes: [{x: parseInt(Math.random() * gridSize), y: parseInt(Math.random() * gridSize)}],
+    score: 0,
 };
 
 socket.emit("joinGame", player);
 
 document.onkeydown = function (event) {
-	event = event || window.event;
-	if (event.keyCode === player.keyCodes[0])
-		player.directionRequest = 1;
-	if (event.keyCode === player.keyCodes[1])
-		player.directionRequest = 2;
-	if (event.keyCode === player.keyCodes[2])
-		player.directionRequest = 3;
-	if (event.keyCode === player.keyCodes[3])
-		player.directionRequest = 4;
-	socket.emit("keyPress", player.directionRequest);
+    event = event || window.event;
+    if (event.keyCode === player.keyCodes[0])
+        player.directionRequest = 1;
+    if (event.keyCode === player.keyCodes[1])
+        player.directionRequest = 2;
+    if (event.keyCode === player.keyCodes[2])
+        player.directionRequest = 3;
+    if (event.keyCode === player.keyCodes[3])
+        player.directionRequest = 4;
+    socket.emit("keyPress", player.directionRequest);
 };
 
 window.onresize = function () {
-	displaySize = display.width = display.height = Math.min(window.innerWidth, window.innerHeight) * 2;
-	blockSize = displaySize / gridSize;
-	display.style.width = (displaySize / 2) + "px";
-	display.style.height = (displaySize / 2) + "px";
+    displaySize = display.width = display.height = Math.min(window.innerWidth, window.innerHeight) * 2;
+    blockSize = displaySize / gridSize;
+    display.style.width = (displaySize / 2) + "px";
+    display.style.height = (displaySize / 2) + "px";
 };
 
 function frame() {
-	context.clearRect(0, 0, displaySize, displaySize);
-	context.fillStyle = "#8BC34A";
-	context.beginPath();
-	context.rect(apple.x * blockSize, apple.y * blockSize, blockSize, blockSize);
-	context.fill();
-	for (let key in snakes)
-		drawSnake(snakes[key]);
+    context.clearRect(0, 0, displaySize, displaySize);
+    context.fillStyle = "#8BC34A";
+    context.beginPath();
+    context.rect(apple.x * blockSize, apple.y * blockSize, blockSize, blockSize);
+    context.fill();
+    for (let key in snakes)
+        drawSnake(snakes[key]);
 }
 
 function drawSnake(snake) {
-	for (let i = snake.nodes.length - 1; i >= 0; i--) {
-		if (i === 0)
-			context.fillStyle = "#FFFFFF";
-		else if (snake.nodes[i].x % 2 === snake.nodes[i].y % 2)
-			context.fillStyle = colors[snake.color][1];
-		else
-			context.fillStyle = colors[snake.color][0];
-		context.beginPath();
-		context.rect(snake.nodes[i].x * blockSize, snake.nodes[i].y * blockSize, blockSize, blockSize);
-		context.fill();
-	}
+    for (let i = snake.nodes.length - 1; i >= 0; i--) {
+        if (i === 0)
+            context.fillStyle = "#FFFFFF";
+        else if (snake.nodes[i].x % 2 === snake.nodes[i].y % 2)
+            context.fillStyle = colors[snake.color][1];
+        else
+            context.fillStyle = colors[snake.color][0];
+        context.beginPath();
+        context.rect(snake.nodes[i].x * blockSize, snake.nodes[i].y * blockSize, blockSize, blockSize);
+        context.fill();
+    }
 }
--- server.js
+++ server.js
@@ -1,17 +1,17 @@
-const express = require("express")
-
+const express = require("express");
 const app = express();
 const port = process.env.PORT || 8080;
+const server = require('http').createServer(app);
+const io = require("socket.io")(server);
 
 app.use(express.static(__dirname + "/client"));
-app.all("*", function(req, res) {
-	res.redirect("/");
+app.all("*", function (req, res) {
+    res.redirect("/");
 });
 
-const server = app.listen(port, function () {
-	console.log("Server running at port %s", port);
+server.listen(port, function () {
+    console.log("Server running at port %s", port);
 });
-const io = require("socket.io") (server);
 
 const gridSize = 40;
 
@@ -19,82 +19,79 @@ let apple = {x: parseInt(Math.random() * gridSize), y: parseInt(Math.random() * 
 let clients = {};
 let snakes = {};
 
-io.sockets.on("connection", function(client) {
-	client.id = randomString(32);
-	client.isPlaying = false;
-	clients[client.id] = client;
-	console.log("Player " + client.id + " connected");
-	client.on("joinGame", function(snake) {
-		snakes[client.id] = snake;
-		client.isPlaying = true;
-		console.log("Player " + client.id + " joined the game");
-	});
-	client.on("keyPress", function(direction) {
-		if (client.isPlaying)
-			snakes[client.id].directionRequest = direction;
-	});
-	client.on("disconnect", function () {
-		delete clients[client.id];
-		delete snakes[client.id];
-		console.log("Player " + client.id + " left the game");
-	});
+io.on("connection", function (client) {
+    client.id = randomString(32);
+    client.isPlaying = false;
+    clients[client.id] = client;
+    client.on("joinGame", function (snake) {
+        snakes[client.id] = snake;
+        client.isPlaying = true;
+    });
+    client.on("keyPress", function (direction) {
+        if (client.isPlaying)
+            snakes[client.id].directionRequest = direction;
+    });
+    client.on("disconnect", function () {
+        delete clients[client.id];
+        delete snakes[client.id];
+    });
 });
 
 setInterval(function () {
-	for (let key in snakes) {
-		updateSnake(snakes[key]);
-	};
-	for (let key in clients) {
-		clients[key].emit('update', {apple: apple, snakes: snakes});
-	};
+    for (let key in snakes) {
+        updateSnake(snakes[key]);
+    }
+    for (let key in clients) {
+        clients[key].emit('update', {apple: apple, snakes: snakes});
+    }
 }, 40);
 
-function updateSnake(snake) {
-	if (snake.direction === 0 || snake.direction % 2 !== snake.directionRequest % 2)
-		snake.direction = snake.directionRequest;
-	let x = snake.nodes[0].x;
-	let y = snake.nodes[0].y;
-	for (let i = 1; i < snake.nodes.length; i++)
-		if (x === snake.nodes[i].x && y === snake.nodes[i].y)
-			; // do nothing
-	if (x === apple.x && y === apple.y) {
-		apple = {
-			x: parseInt(Math.random() * gridSize),
-			y: parseInt(Math.random() * gridSize),
-		};
-		snake.score++;
-		let end = snake.nodes[snake.nodes.length - 1];
-		snake.nodes.push({x: end.x, y: end.y});
-		snake.nodes.push({x: end.x, y: end.y});
-		snake.nodes.push({x: end.x, y: end.y});
-	}
-	switch (snake.direction) {
-		case 1:
-			snake.nodes.unshift({x: x - 1, y: y});
-			snake.nodes.pop();
-			break;
-		case 2:
-			snake.nodes.unshift({x: x, y: y - 1});
-			snake.nodes.pop();
-			break;
-		case 3:
-			snake.nodes.unshift({x: x + 1, y: y});
-			snake.nodes.pop();
-			break;
-		case 4:
-			snake.nodes.unshift({x: x, y: y + 1});
-			snake.nodes.pop();
-			break;
-	}
-	snake.nodes[0].x = ((snake.nodes[0].x % gridSize) + gridSize) % gridSize;
-	snake.nodes[0].y = ((snake.nodes[0].y % gridSize) + gridSize) % gridSize;
-}
+let updateSnake = function (snake) {
+    if (snake.direction === 0 || snake.direction % 2 !== snake.directionRequest % 2)
+        snake.direction = snake.directionRequest;
+    let x = snake.nodes[0].x;
+    let y = snake.nodes[0].y;
+    for (let i = 1; i < snake.nodes.length; i++)
+        if (x === snake.nodes[i].x && y === snake.nodes[i].y)
+            ; // do nothing
+    if (x === apple.x && y === apple.y) {
+        apple = {
+            x: parseInt(Math.random() * gridSize),
+            y: parseInt(Math.random() * gridSize),
+        };
+        snake.score++;
+        let end = snake.nodes[snake.nodes.length - 1];
+        snake.nodes.push({x: end.x, y: end.y});
+        snake.nodes.push({x: end.x, y: end.y});
+        snake.nodes.push({x: end.x, y: end.y});
+    }
+    switch (snake.direction) {
+        case 1:
+            snake.nodes.unshift({x: x - 1, y: y});
+            snake.nodes.pop();
+            break;
+        case 2:
+            snake.nodes.unshift({x: x, y: y - 1});
+            snake.nodes.pop();
+            break;
+        case 3:
+            snake.nodes.unshift({x: x + 1, y: y});
+            snake.nodes.pop();
+            break;
+        case 4:
+            snake.nodes.unshift({x: x, y: y + 1});
+            snake.nodes.pop();
+            break;
+    }
+    snake.nodes[0].x = ((snake.nodes[0].x % gridSize) + gridSize) % gridSize;
+    snake.nodes[0].y = ((snake.nodes[0].y % gridSize) + gridSize) % gridSize;
+};
 
-function randomString(length) {
-	let text = "";
-	var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-	for(var i = 0; i < length; i++) {
-		text += possible.charAt(parseInt(Math.random() * possible.length));
-	}
-	return text;
-}
+let randomString = function (length) {
+    let text = "";
+    let possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    for (let i = 0; i < length; i++) {
+        text += possible.charAt(parseInt(Math.random() * possible.length));
+    }
+    return text;
+};
